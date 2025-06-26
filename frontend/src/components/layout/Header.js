61ac6f78770c5dfe6e60eead6c30c27cbb5138ee
tizzifona <tizzifona@gmail.com>
2025-06-26 13:08:25 +0200

    feat: added styles for Header and Footer

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './Header.module.css';
+import logo from '../../images/logo.png';
 
 const Header = () => {
   return (
-    <header>
-      <h1>Electronify</h1>
-      <nav>
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <Link to="/">
+        <img src={logo} alt="Electronify" />
+          <h1>Electronify</h1>
+        </Link>
+      </div>
+      
+      <nav className={styles.nav}>
         <Link to="/">Home</Link>
         <Link to="/products">Products</Link>
         <Link to="/categories">Categories</Link>
